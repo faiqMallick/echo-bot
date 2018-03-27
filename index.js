c7dfcaf668ec957cea697a02b930f9a4fc601ff0
Faiq Shamim <Faiq.Shamim@SHMA.COM>
2018-03-27 18:08:02 +0500

    Url Updated 11

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function handleEvent(event) {
    console.log("pappu12323");
 
 
-   http.get('http://66.228.117.22:50629/RestTest1/RestTest/', (resp) => {
+   https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
     // let data = '';
     console.log("httpin");
 
@@ -52,8 +52,9 @@ function handleEvent(event) {
       body += data;
     });
     resp.on("end", () => {
-     // body = JSON.parse(body);
+      body = JSON.parse(body);
       console.log(body);
+       echo = { type: 'text', text: "You said: " + body };
     });
     // A chunk of data has been recieved.
     // resp.on('data', (chunk) => {
