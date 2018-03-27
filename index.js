5dd63e43da0291b9205a3e675fcbbfbf9d8bc7a9
Faiq Shamim <Faiq.Shamim@SHMA.COM>
2018-03-27 17:21:06 +0500

    Url Updated 3

--- index.js
+++ index.js
@@ -45,16 +45,16 @@ function handleEvent(event) {
     let data = '';
     console.log("httpin");
 
-
+    console.log(JSON.parse(resp));
     // A chunk of data has been recieved.
-    resp.on('data', (chunk) => {
-      data += chunk;
-    });
+    // resp.on('data', (chunk) => {
+    //   data += chunk;
+    // });
    
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
-      console.log(JSON.parse(data).explanation);
-    });
+    // // The whole response has been received. Print out the result.
+    // resp.on('end', () => {
+    //   console.log(JSON.parse(data).explanation);
+    // });
    
   }).on("error", (err) => {
     console.log("Error: " + err.message);
