7c9dcdaf48bacc620d817db358a97f0ce53b29e0
Faiq Shamim <Faiq.Shamim@SHMA.COM>
2018-03-28 20:53:27 +0500

    Updated 42

--- index.js
+++ index.js
@@ -81,12 +81,9 @@ function handleEvent(event) {
 request.post({
   headers: {'content-type' : 'application/x-www-form-urlencoded'},
   url:     'http://66.228.117.22/RestTest/',
-  body:   event.message.text //"20060461"
+  body:   event.message.type //"20060461"
 }, function(error, response, body){
-
-  console.log("WS1044 LOG "+event.message.text);
-  console.log("WS1044 LOG "+body);
-  
+  console.log("WS1044LOG "+body);
 });
 
 
@@ -113,8 +110,7 @@ request.post({
 
 
   // use reply API
-  
-  return client.replyMessage(event.replyToken, request.text);
+  return client.replyMessage(event.replyToken, body);
 }
 
 // listen on port
